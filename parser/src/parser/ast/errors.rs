f430350ba5e429e2ce1ea1cf70d150c413bff483
El-PowerPtr <moiseselpower@gmail.com>
2025-07-30 20:41:20 -0400

    ok, another merge message

--- parser/src/parser/ast/errors.rs
+++ parser/src/parser/ast/errors.rs
@@ -1,7 +1,17 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+pub enum AstErr {}
+=======
+>>>>>>> a22607f (chore!: rowan removed from the project. Parser implementation needed.)
 pub enum AstErr {
     #[error("Terminal node: {0} cannot have children")]
     TerminalNodeChildAdition(String),
 }
+<<<<<<< HEAD
+=======
+>>>>>>> 6613d25 (chore!: rowan removed from the project. Parser implementation needed.)
+>>>>>>> a22607f (chore!: rowan removed from the project. Parser implementation needed.)
