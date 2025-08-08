a16882dae4866df6a2427143536c5db0b087c905
El-PowerPtr <moiseselpower@gmail.com>
2025-08-08 01:24:47 -0400

    feat: traits for ast, builder, nodes and dialect defined

--- parser/src/parser/ast/ast_trait.rs
+++ parser/src/parser/ast/ast_trait.rs
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-use super::errors::AstErr;
-use super::nodes::TreeNode;
-use crate::parser::grammar::dialect::Dialect;
-=======
-<<<<<<< HEAD
-use crate::parser::ast::ast::SyntaxKind;
-use crate::parser::ast::errors::AstErr;
-use crate::parser::lexer::Token;
-use std::fmt::Display;
->>>>>>> a22607f (chore!: rowan removed from the project. Parser implementation needed.)
-
-pub trait Ast<D>
-where
-    D: Dialect,
-{
-<<<<<<< HEAD
-=======
-    fn diff(&self, other: &Self) -> Self;
-    fn as_node(&self) -> N;
-    fn from(root: &N) -> Self;
-=======
 use super::errors::AstErr;
 use super::nodes::TreeNode;
 use crate::parser::grammar::dialect::Dialect;
@@ -28,14 +6,9 @@ pub trait Ast<D>
 where
     D: Dialect,
 {
->>>>>>> a22607f (chore!: rowan removed from the project. Parser implementation needed.)
     fn root(&self) -> Option<Box<TreeNode<D>>>;
     fn take_root(&mut self, node: TreeNode<D>) -> Result<(), AstErr>;
     fn diff(&self, other: &Self) -> Vec<Box<Self>>;
     fn from_node(seed: TreeNode<D>) -> Self;
     fn new() -> Self;
-<<<<<<< HEAD
-=======
->>>>>>> 6613d25 (chore!: rowan removed from the project. Parser implementation needed.)
->>>>>>> a22607f (chore!: rowan removed from the project. Parser implementation needed.)
 }
--- parser/src/parser/ast/mod.rs
+++ parser/src/parser/ast/mod.rs
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
 pub mod ast_builder;
 pub mod ast_trait;
 pub mod errors;
 pub mod nodes;
 pub mod syntax_kind;
-=======
-<<<<<<< HEAD
-pub mod ast;
-pub mod ast_trait;
-pub mod errors;
-pub mod nodes;
-pub mod operators;
-=======
-pub mod ast_builder;
-pub mod ast_trait;
-pub mod errors;
-pub mod nodes;
-pub mod syntax_kind;
->>>>>>> 6613d25 (chore!: rowan removed from the project. Parser implementation needed.)
->>>>>>> a22607f (chore!: rowan removed from the project. Parser implementation needed.)
