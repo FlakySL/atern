efb8d3e01f083401fd579d204421dd40218e6a34
El-PowerPtr <moiseselpower@gmail.com>
2025-08-21 02:32:34 -0400

    chore(grammar): dialect checking method commented temporaly

--- parser/src/parser/grammar/dialect.rs
+++ parser/src/parser/grammar/dialect.rs
@@ -1,8 +1,7 @@
-use crate::parser::ast::errors::AstErr;
-use crate::parser::ast::nodes::TreeNode;
+//use crate::parser::ast::errors::AstErr;
 
 ///This trait deals with the grammar of the SQL dialect we want to parse.
 ///We proly need to add more methods in the future.
-pub trait Dialect: Sized {
-    fn are_compatible(child: &TreeNode<Self>, parent: &TreeNode<Self>) -> Result<(), AstErr>;
+pub trait Dialect<N>: Sized {
+    //fn are_compatible(child: &N, parent: &N) -> Result<(), AstErr>;
 }
